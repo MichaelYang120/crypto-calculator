db9646beb9f3039f79a0a29722dba0452cb3dd36
MichaelYang120 <michaelyangiscoding@gmail.com>
2023-04-18 19:42:48 -0500

    added styling, form, and input

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -4,7 +4,11 @@ import { api } from '../Api/ApiRequest'
 export default function Main() {
     console.log(api())
     return (
-        <div>This is the main container</div>
+        <div className='maincontainer'>
+            <form className='findtickerform'>
+                <input type="text" placeholder='enter your crypto ticker' />
+            </form>
+        </div>
     )
 
 }
