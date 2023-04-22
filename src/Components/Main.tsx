035773646333a966d2ecba783843801136118eae
MichaelYang120 <michaelyangiscoding@gmail.com>
2023-04-22 10:26:59 -0500

    filter prices with 0.00000000

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -6,7 +6,6 @@ const debug = true;
 export default function Main() {
     const [crypto, setCrypto] = useState([]);
 
-
     useEffect(() => {
         async function getcrypto() {
             var result: any = await api()
@@ -16,26 +15,32 @@ export default function Main() {
         if (debug === true) {
             console.log(crypto)
         }
-
-
+        
+        
     }, [])
 
     return (
         <>
             {
-                crypto.map(({ symbol, bidPrice, askPrice }) =>
-                    <div className="cryptocontainer">
-                        <h2 className='cryptoname'>{symbol}</h2>
-                            <div className='cryptoinnercontainer'>
-                                <div>
-                                    <p>Asking Price : {askPrice}</p>
-                                </div>
-                                <div>
-                                    <p>Bid Price : {bidPrice}</p>
-                                </div>
-                            </div>
-                    </div>
-                )
+                crypto.map(val => {
+                    if (val["askPrice"] !== "0.00000000" && val["bidPrice"] !== "0.00000000") {
+                        return (
+                            <>
+                                <div className="cryptocontainer">
+                                    <h3 className='cryptoname'>{val["symbol"]}</h3>
+                                    <div className='cryptoinnercontainer'>
+                                        <div>
+                                            <p>Asking Price : {val["askPrice"]}</p>
+                                        </div>
+                                        <div>
+                                            <p>Bid Price : {val["bidPrice"]}</p>
+                                        </div>
+                                    </div>
+                                </div>                            
+                            </>
+                        )
+                    }
+                })
             }
         </>
     )
