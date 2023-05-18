29977317c8c2cc136ec9897fba25af86f292195e
MichaelYang120 <michaelyangiscoding@gmail.com>
2023-05-17 21:45:39 -0500

    added to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Main from './Components/Main';
+import Header from './Components/Header';
 
 function App() {
   return (
     <>
+      <Header/>
       <Main/>
     </>
   );
