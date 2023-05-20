6ff64c1cdb3d76a3fee91f8989eecc3ad9bea8c5
MichaelYang120 <michaelyangiscoding@gmail.com>
2023-05-19 22:06:19 -0500

    added new file named Single.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import './App.css';
 import Main from './Components/Main';
 import Header from './Components/Header';
 import Search from './Components/Search';
+import Single from './Components/Single';
 
 function App() {
   return (
     <>
       <Header/>
       <Search/>
-      <Main/>
+      <Single/>
+      {/* <Main/> */}
     </>
   );
 }
