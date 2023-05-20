5cfe7d300ccbcc1f37301166bbd36a5fe246111e
MichaelYang120 <michaelyangiscoding@gmail.com>
2023-05-19 22:46:19 -0500

    converted indents to tabs

--- src/Components/Single.tsx
+++ src/Components/Single.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 
 export default function Single() {
-    function getCurrentURL () {
-        return window.location.href
-      }
-    const url = getCurrentURL()
-    console.log(url)
+	function getCurrentURL () {
+		return window.location.href
+	}
+	const url = getCurrentURL()
+	console.log(url)
 
+	
 
 	return (
 		<>
-            <div>
-                Single symbol
-            </div>
+			<div>
+				Single symbol
+			</div>
 		</>
 	);
 }
